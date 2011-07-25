d107af83f09e0fadc68fbd2ecbe449adbbd9a273
jondavidjohn(work) <me@jondavidjohn.com>
2011-07-25 16:54:15 -0500

    init-commit

---
+++ example_lang_files/en/test.php
@@ -0,0 +1,9 @@
+<?
+/***
+ * Example Lang file to be used to test success
+ */
+return array(
+    'hello' => 'Hello :name',
+    'something'=> 'something :name!',
+    'test'=> array('hello' => 'Hello', 'something' => 'Plop') // Group
+);
