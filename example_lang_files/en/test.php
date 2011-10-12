75803df3ff3b5af719d9479e8772741a2b25c9b6
Jonathan Johnson <me@jondavidjohn.com>
2011-10-12 09:04:33 -0500

    Updated example lang file

--- example_lang_files/en/test.php
+++ example_lang_files/en/test.php
@@ -3,7 +3,8 @@
  * Example Lang file to be used to test success
  */
 return array(
-    'hello' => 'Hello :name',
+    'hello' => 'Hello, my name is :name and my dog is a :dog',
     'something'=> 'something :name!',
+    'another' => 'some more :stuff!!!',
     'test'=> array('hello' => 'Hello', 'something' => 'Plop') // Group
 );
